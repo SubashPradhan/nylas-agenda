8f199ced791a45b53749bd954d41feaee8f8903d
Subash Pradhan <subash@ip-192-168-2-5.ec2.internal>
2021-06-10 09:59:20 +0200

    initializing projecy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Agenda</h1>
+      <nylas-agenda id="a2aac1d6-9794-4aff-b4d0-0cc5a4279e47" > </nylas-agenda>
     </div>
   );
 }
