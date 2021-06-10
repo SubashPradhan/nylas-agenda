e87f5305e4dcb7b3789df4c85e2a8011e1fe3a15
Subash Pradhan <subash@ip-192-168-2-5.ec2.internal>
2021-06-10 10:12:31 +0200

    test

--- src/App.js
+++ src/App.js
@@ -6,8 +6,6 @@ export default function App() {
     <div className="App">
       <h1>Nylas Agenda</h1>
       <nylas-agenda
-        allow_date_change="true"
-        hide_ticks="false"
         id="a2aac1d6-9794-4aff-b4d0-0cc5a4279e47"
       >
         {" "}
