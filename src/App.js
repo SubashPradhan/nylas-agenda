db168ec353cff4e278ec773c350ab952a35160b6
Subash Pradhan <subash@ip-192-168-2-5.ec2.internal>
2021-06-10 10:05:02 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import React from "react";
+import "@nylas/components-agenda";
 
-
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1>Hello Agenda</h1>
-      <nylas-agenda id="a2aac1d6-9794-4aff-b4d0-0cc5a4279e47" > </nylas-agenda>
+      <h1>Nylas Agenda</h1>
+      <nylas-agenda
+        allow_date_change="true"
+        hide_ticks="false"
+        id="a2aac1d6-9794-4aff-b4d0-0cc5a4279e47"
+      >
+        {" "}
+      </nylas-agenda>
     </div>
   );
 }
 
-export default App;
